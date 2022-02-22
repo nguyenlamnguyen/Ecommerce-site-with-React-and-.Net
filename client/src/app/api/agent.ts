117f2f05ac4b74bc725483ba984790801efb6cc2
Nguyen Lam Nguyen <infernonguyen118@gmail.com>
2022-02-22 11:10:05 +0700

    Paging, sort, filter

--- client/src/app/api/agent.ts
+++ client/src/app/api/agent.ts
@@ -1,6 +1,7 @@
 import axios, { AxiosError, AxiosResponse } from "axios";
 import { toast } from "react-toastify";
 import { history } from "../..";
+import { PaginatedResponse } from "../models/pagination";
 
 // Add delay for loading screen
 const sleep = () => new Promise(resolve => setTimeout(resolve, 300));
@@ -12,6 +13,11 @@ const responseBody = (response: AxiosResponse) => response.data;
 
 axios.interceptors.response.use(async response =>{
     await sleep();
+    const pagination = response.headers['pagination'];
+    if(pagination) {
+        response.data = new PaginatedResponse(response.data, JSON.parse(pagination));
+        return response;
+    }
     return response;
 }, (error: AxiosError) => {
     const {data, status} = error.response!; // ! overwrite type safety
@@ -45,15 +51,16 @@ axios.interceptors.response.use(async response =>{
 })
 
 const requests = {
-    get: (url: string) => axios.get(url).then(responseBody),
+    get: (url: string, params?: URLSearchParams) => axios.get(url, {params}).then(responseBody),
     post: (url: string, body: {}) => axios.post(url, body).then(responseBody),
     put: (url: string, body: {}) => axios.put(url, body).then(responseBody),
     delete: (url: string) => axios.delete(url).then(responseBody),
 }
 
 const Catalog = {
-    list: () => requests.get('products'),
-    details: (id: number) => requests.get(`products/${id}`)
+    list: (params: URLSearchParams) => requests.get('products', params),
+    details: (id: number) => requests.get(`products/${id}`),
+    fetchFilterrs: () => requests.get('products/filters')
 }
 
 const TestErrors = {
